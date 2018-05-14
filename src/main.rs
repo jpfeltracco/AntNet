a5a4d8fa970e157ddff8d9a126fb2dbaf9138ac8
Jeremy <jpfeltracco@gatech.edu>
2018-05-14 01:16:53 -0400

    Trying to remove sdl / refractor into standalone

--- src/main.rs
+++ src/main.rs
@@ -97,37 +97,58 @@ fn texture_block<'a>(canvas: &mut Canvas<Window>, texture_creator: &'a TextureCr
     return texture;
 }
 
+struct MySys {
+    sdl_context: sdl2::Sdl,
+    canvas: sdl2::render::Canvas<sdl2::video::Window>,
+}
+
+impl MySys {
+    fn new() -> MySys {
+        let context = sdl2::init().unwrap();
+        let vid_subsys = context.video().unwrap();
+        let win = vid_subsys
+            .window("rust-sdl2 demo: Game of Life",
+                    SQUARE_SIZE*PLAYGROUND_WIDTH,
+                    SQUARE_SIZE*PLAYGROUND_HEIGHT)
+            .position_centered()
+            .build()
+            .unwrap();
+
+        let mut canv = win.into_canvas()
+            .target_texture()
+            .present_vsync()
+            .build().unwrap();
+
+        let mut my_sys = MySys{
+            sdl_context: context,
+            canvas: canv,
+        };
+
+        println!("Using SDL_Renderer \"{}\"", my_sys.canvas.info().name);
+
+        my_sys.canvas.set_draw_color(color::get(color::Type::White));
+        // clears the my_sys.canvas with the color we set in `set_draw_color`.
+        my_sys.canvas.clear();
+        // However the my_sys.canvas has not been updated to the window yet, everything has been processed to
+        // an internal buffer, but if we want our buffer to be displayed on the window, we need to call
+        // `present`. We need to call this everytime we want to render a new frame on the window.
+        my_sys.canvas.present();
+
+        my_sys
+    }
+
+    fn clear(&mut self) {
+        self.canvas.set_draw_color(Color::RGB(255, 255, 255));
+        self.canvas.clear();
+    }
+
+    fn show(&mut self) {
+        self.canvas.present();
+    }
+}
+
 pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-
-    // the window is the representation of a window in your operating system,
-    // however you can only manipulate properties of that window, like its size, whether it's
-    // fullscreen, ... but you cannot change its content without using a Canvas or using the
-    // `surface()` method.
-    let window = video_subsystem
-        .window("rust-sdl2 demo: Game of Life",
-                SQUARE_SIZE*PLAYGROUND_WIDTH,
-                SQUARE_SIZE*PLAYGROUND_HEIGHT)
-        .position_centered()
-        .build()
-        .unwrap();
-
-    // the canvas allows us to both manipulate the property of the window and to change its content
-    // via hardware or software rendering. See CanvasBuilder for more info.
-    let mut canvas = window.into_canvas()
-        .target_texture()
-        .present_vsync()
-        .build().unwrap();
-
-    println!("Using SDL_Renderer \"{}\"", canvas.info().name);
-    canvas.set_draw_color(color::get(color::Type::White));
-    // clears the canvas with the color we set in `set_draw_color`.
-    canvas.clear();
-    // However the canvas has not been updated to the window yet, everything has been processed to
-    // an internal buffer, but if we want our buffer to be displayed on the window, we need to call
-    // `present`. We need to call this everytime we want to render a new frame on the window.
-    canvas.present();
+    let mut my_sys = MySys::new();
 
     let mut world = World::new();
     world.register::<component::Pos>();
@@ -164,26 +185,24 @@ pub fn main() {
     // let mut delta = world.write_resource::<DeltaTime>();
 
     world.add_resource(component::ColorPos{ vec: Vec::new() });
-
     world.add_resource(ArrowKeyEvent(Vec::new()));
-    
     world.add_resource(component::CamPos(0, 0));
 
     let mut render_sys = RenderSystem;
     let mut keyboard_sys = KeyboardInputSystem;
     let mut cam_follow_sys = CameraFollowerSystem;
 
-    // this struct manages textures. For lifetime reasons, the canvas cannot directly create
+    // this struct manages textures. For lifetime reasons, the my_sys.canvas cannot directly create
     // textures, you have to create a `TextureCreator` instead.
-    let texture_creator : TextureCreator<_> = canvas.texture_creator();
+    let texture_creator : TextureCreator<_> = my_sys.canvas.texture_creator();
 
     let mut game = ant_sim::AntGame::new();
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut event_pump = my_sys.sdl_context.event_pump().unwrap();
 
     let mut tex_vec = Vec::new();
 
     for n in 0..color::len() {
-        tex_vec.push(texture_block(&mut canvas, &texture_creator, color::enumify(n)));
+        tex_vec.push(texture_block(&mut my_sys.canvas, &texture_creator, color::enumify(n)));
     }
 
     'running: loop {
@@ -222,31 +241,27 @@ pub fn main() {
         keyboard_sys.run_now(&world.res);
         cam_follow_sys.run_now(&world.res);
 
-        // get data out of systems
         let color_pos = world.read_resource::<component::ColorPos>();
-
         let cam_pos = world.read_resource::<component::CamPos>();
 
-        println!("Cam component::Pos {:?}", *cam_pos);
-        println!("Canvas size? {:?}", canvas.output_size());
-
         // rendering
-        canvas.set_draw_color(Color::RGB(255, 255, 255));
-        canvas.clear();
+        my_sys.clear();
 
         for (bc, pos) in color_pos.vec.clone() {
             let x_pos_unit = pos.0 - cam_pos.0;
             let y_pos_unit = pos.1 - cam_pos.1;
 
-            let canvas_size = canvas.output_size().unwrap();
+            let canvas_size = my_sys.canvas.output_size().unwrap();
             let x_pos_px = (x_pos_unit * SQUARE_SIZE as i32) + (canvas_size.0 as i32 / 2);
             let y_pos_px = (y_pos_unit * SQUARE_SIZE as i32) + (canvas_size.1 as i32 / 2);
 
             let rect = Rect::new(x_pos_px, y_pos_px, SQUARE_SIZE, SQUARE_SIZE);
 
-            let _res = canvas.copy(&tex_vec[color::indexify(bc.id)], None, rect);
+            let _res = my_sys.canvas.copy(&tex_vec[color::indexify(bc.id)], None, rect);
         }
 
-        canvas.present();
+        bla = tex_vec as u32;
+
+        my_sys.show();
     }
 }
